f783f6d4f92c139d3bce6fd261a2bff02fe2218f
Taras Stasyuk <taras-st@MacBook-Air.local>
2025-03-04 00:14:47 +0300

    fix

--- homecheck/fintech/models.py
+++ homecheck/fintech/models.py
@@ -6,8 +6,8 @@ class Plan(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
+    check_limit = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
