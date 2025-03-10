679dbe8e6e442ba50c28b43ae11a77b5c176d930
Taras Stasyuk <taras-st@MacBook-Air.local>
2025-03-10 19:08:12 +0300

    fix

--- homecheck/fintech/urls.py
+++ homecheck/fintech/urls.py
@@ -1,7 +1,13 @@
 from rest_framework import routers
 from .views import PlanViewSet
+from django.urls import path
+from .views import get_invoice
 
 router = routers.DefaultRouter()
 router.register(r'plans', PlanViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('webhook/', get_invoice, name='get_invoice'),
+]
--- homecheck/fintech/views.py
+++ homecheck/fintech/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Plan
 from .serializers import PlanSerializer
+from telebot import TeleBot
+from telebot import types
+from django.conf import settings
+from django.http import JsonResponse
 
 class PlanViewSet(viewsets.ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
 
+
+
+def get_invoice(request):
+    provider_token = settings.PROVIDER_TOKEN
+    bot = TeleBot(settings.BOT_TOKEN)
+    response = bot.create_invoice_link("Покупка","Тестовый","true",provider_token,"rub",[types.LabeledPrice('Покупка',5 * 100)])
+    response = JsonResponse({"invoice_url": response.invoice_url})
+    print(response)
+    return response
